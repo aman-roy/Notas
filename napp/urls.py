aab4fa3a50151184b0c0baeb94436eaec6a92e33
Aadesh Mirajkar <mirajkaraadesh@gmail.com>
2019-09-26 10:49:20 +0530

    new template implemented

--- napp/urls.py
+++ napp/urls.py
@@ -1,10 +1,13 @@
 from napp import views
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
+
 
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about/$', views.about, name='about'),
     url(r'^contact/$', views.contact, name='contact'),
+    url(r"^logout/$", views.logout_request, name="logout"),
 ]
--- napp/views.py
+++ napp/views.py
@@ -1,15 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import logout
 from napp.models import Author
 
 # Create your views here.
 def index(request):
-	return render(request, 'unauth/index.html')
+	if request.user.is_authenticated:
+		return render(request, 'index.html')
+	return render(request, 'home.html')
 
 
 def about(request):
-	return render(request, 'unauth/about.html')
+	return render(request, 'home.html')
 
 
 def contact(request):
-	return render(request, 'unauth/contact.html')
+	return render(request, 'contact.html')
+
+def logout_request(request):
+    logout(request)
+    return redirect("index")
